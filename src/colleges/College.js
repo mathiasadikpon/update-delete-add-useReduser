8c500c0998c9b8f0647cddd5ad9826414e21bd37
Mathias Adikpon <mathiasadikpon@gmail.com>
2025-03-05 07:31:00 -0500

    Refactor College component and update COLLEGESDATA structure for improved data representation

--- src/colleges/College.js
+++ src/colleges/College.js
@@ -1,17 +1,33 @@
-import React from 'react'
-import {Card, CardBody, CardImg, CardText, CardTitle} from 'reactstrap';
-const College = (college) => {
-    const {name, city, state, country, no_of_students, courses, website_url} = college;
+import React from "react";
+import { Card, CardBody, CardImg, CardText, CardTitle } from "reactstrap";
+const College = ({ college }) => {
+  const {
+    name,
+    city,
+    state,
+    country,
+    no_of_students,
+    courses,
+    website,
+    logo_path,
+  } = college;
   return (
     <Card>
-        <CardImg top width="100%" src="https://via.placeholder.com/150" alt="Card image cap" />
-        <CardBody>
-            <CardTitle tag="h5">Card title</CardTitle>
-            <CardText>Some quick example text to build on the card title and make up the bulk of the card's content.</CardText>
-        </CardBody>
-      
+      <CardImg top width="100%" src={logo_path} alt={name} />
+      <CardBody>
+        <CardTitle tag="h5">{name}</CardTitle>
+        <CardText>
+          <strong>Location:</strong> {city}, {state}, {country} <br />
+          <strong>Students:</strong> {no_of_students.toLocaleString()} <br />
+          <strong>Courses:</strong> {courses.join(", ")} <br />
+          <strong>Website:</strong>{" "}
+          <a href={website} target="_blank" rel="noopener noreferrer">
+            {website}
+          </a>
+        </CardText>
+      </CardBody>
     </Card>
-  )
-}
+  );
+};
 
 export default College;
